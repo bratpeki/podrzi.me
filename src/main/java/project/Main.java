50e2cae5da18e7f792c10e2ba37c45d81f32e5fb
Yorth <lukaiorthr@gmail.com>
2025-07-09 14:01:10 +0200

    Uredjivanje koda

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -5,33 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.handler.UserRoleAuthorizationInterceptor;
 
 import java.util.List;
 
 @SpringBootApplication
-@RestController
-@RequestMapping("/api")
 public class Main {
-
-	private final UserRepository userRepository;
-
-	public Main (UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
-	}
-
-	@GetMapping("/hello")
-	public String Hello() {
-		return "hi";
-	}
-
-	@GetMapping("/getusers")
-	public List<String> GetUsers() {
-		List<User> list = userRepository.findAll();
-		return list.stream().map(User::GetDisplayname).toList();
 	}
 }
---
+++ src/main/java/project/UserAPI.java
@@ -0,0 +1,23 @@
+package project;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserAPI {
+    private final UserRepository userRepository;
+
+    public UserAPI (UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/getusers")
+    public List<String> GetUsers() {
+        List<User> list = userRepository.findAll();
+        return list.stream().map(User::GetDisplayname).toList();
+    }
+}
