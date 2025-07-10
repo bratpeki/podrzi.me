bd543874cfacd79520d6445b435169c9cf34e081
GabrijelDjuric <gabrijel.djuric@student.etf.unibl.org>
2025-07-10 17:36:19 +0200

    Dodavanje stranice "About Us"

---
+++ React/src/AboutUsPage.js
@@ -0,0 +1,40 @@
+// src/pages/AboutUs.js
+import React from 'react';
+
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-container">
+      <h1>O nama</h1>
+      <p>
+        <strong>Podrži.me</strong> je humanitarna platforma nastala iz želje da pomognemo onima kojima je pomoć najpotrebnija.
+        Kroz našu zajednicu, povezujemo ljude velikog srca sa onima koji se bore s izazovima života.
+      </p>
+
+      <p>
+        Naša misija je da stvorimo most povjerenja između donatora i onih kojima je pomoć potrebna,
+        inovativno i sa dubokom vjerom u moć zajedništva.
+      </p>
+
+      <p>
+        Kroz podršku pojedinaca i organizacija, <strong>Podrži.me</strong> omogućava brze, pouzdane i personalizovane kampanje koje
+        imaju stvaran uticaj na živote ljudi.
+      </p>
+
+      <h2>Naše vrijednosti</h2>
+      <ul>
+        <li>Povjerenje i transparentnost</li>
+        <li>Pomoć onima kojima je potrebno</li>
+        <li>Zajedništvo i solidarnost</li>
+        <li>Glas onima koji ga nemaju</li>
+      </ul>
+
+      <p>
+        Zajedno možemo učiniti svijet boljim — jedna podrška u isto vrijeme.
+        Pridruži se pokretu. <strong>Podrži. Pomozi. Promijeni.</strong>
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
