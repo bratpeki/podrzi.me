a193a26a796b7988bee5e149a1ffc4b42f49f667
bratpeki <pkatic2003@gmail.com>
2025-07-16 15:05:56 +0200

    Dodata slika na ViewDonationsPage

--- React/src/pages/ViewDonationsPage.js
+++ React/src/pages/ViewDonationsPage.js
@@ -22,6 +22,7 @@ function ViewDonationsPage() {
 
 		const fetchDons = async () => {
 		  try {
+			  // TODO: Deprekacija, valjda mijenjamo await fetch sa apiRequest
 			  const res = await fetch("http://podrzime.ddns.net:8080/api/donations/getdonationsuser?idUser=" + idUser, {
 			  // const res = await fetch("http://podrzime.ddns.net:8080/api/donations/getdonationsuser?idUser=48", {
 			  method: 'GET',
@@ -42,7 +43,21 @@ function ViewDonationsPage() {
 			// Za potrebe testiranja mnogo elemenata
 			// Ispašće upozorenje za mnogo identičnih key-eva, ali to nam je nebitno
 			// for ( let i = 0; i < 20; i++ ) data.push(data[0]);
+
+			for (let i = 0; i < data.length; i++) {
+				let resImg = await fetch(
+					"http://podrzime.ddns.net:8080/api/images/getprimaryimage?idAction=" + data[i].idAction,
+					{
+						method: 'GET',
+						'token': authState.accessToken,
+					}
+				);
+				let imgResponse = await resImg.text();
+				data[i].img = imgResponse;
+			}
+
 			setDons(data);
+
 
 		  } catch (err) {
 				console.error('Greška pri učitavanju donacija:', err.message);
@@ -79,17 +94,20 @@ function ViewDonationsPage() {
 		{
 			dons.map(
 				donation =>
-				<div key={donation.idDonation} className="bg-white rounded-lg shadow-md p-6 mb-4 border border-gray-200 w-9/12 min-w-max mx-auto">
-					<h3 className="text-xl font-semibold text-gray-800 mb-2">
-						Akcija: {donation.actionName}
-					</h3>
-					<p className="text-gray-700 mb-1">
-						<span className="font-medium">Iznos donacije:</span> {donation.amount} KM
-					</p>
-					<p className="text-gray-600 text-sm">
-						<span className="font-medium">Vrijeme donacije:</span>{" "}
-						{new Date(donation.donationTime).toLocaleString()} {/* Formats the date and time nicely */}
-					</p>
+				<div key={donation.idDonation} className="bg-white rounded-lg shadow-md p-6 mb-4 border border-gray-200 w-9/12 min-w-max mx-auto flex justify-between items-center">
+					<div className="w-2/3">
+						<h3 className="text-xl font-semibold text-gray-800 mb-2">
+							Akcija: {donation.actionName}
+						</h3>
+						<p className="text-gray-700 mb-1">
+							<span className="font-medium">Iznos donacije:</span> {donation.amount} KM
+						</p>
+						<p className="text-gray-600 text-sm">
+							<span className="font-medium">Vrijeme donacije:</span>{" "}
+							{new Date(donation.donationTime).toLocaleString()}
+						</p>
+					</div>
+					<img src={donation.img} alt={`Image for ${donation.actionName}`} className="w-1/3 h-48 object-cover rounded-md ml-4"/> {/* Image explicitly takes 1/3 and has left margin */}
 				</div>
 			)
 		}
