506826e637e22da87f3f702807459fd222ac58cb
Milos Petrovic <darthypyro@gmail.com>
2025-07-31 10:50:38 +0200

    Pregled recenzija

--- React/src/pages/admin/AdminHomePage.js
+++ React/src/pages/admin/AdminHomePage.js
@@ -78,28 +78,27 @@ function AdminHomePage() {
 
             <div className="mt-16"></div>
 
-            <div className="flex flex-col bg-white rounded-lg shadow-md w-2/5 h-full p-8 mt-20 items-center justify-center p-20">
+            <div className="flex flex-col bg-white rounded-lg shadow-md w-2/5 h-full  mt-20 items-center justify-center p-20">
 
                 <h1 className="text-4xl font-bold text-cyan-900 mb-8 drop-shadow-md"> Administratorski panel </h1>
 
-                { /* TODO: Peki */}
                 <Link to={"/admin/viewAccounts"} className="button-style text-3xl w-72 h-20 mb-8 flex items-center justify-center">
                     Pregled korisnika
                 </Link>
 
-                { /* TODO: Pero */}
                 <Link to={"/admin/viewActions"} className="button-style text-3xl w-72 h-20 mb-8 flex items-center justify-center">
                     Pregled akcija
                 </Link>
 
-                { /* TODO: Peki */}
                 <Link to={"/admin/viewReports"} className="button-style text-3xl w-72 h-20 mb-8 flex items-center justify-center">
                     Pregled prijava
                 </Link>
 
-                { /* TODO */}
-                <Link to={"/admin/viewRefunds"} className="button-style text-3xl w-72 h-20 flex items-center justify-center text-center">
+                <Link to={"/admin/viewRefunds"} className="button-style text-3xl w-72 h-20 mb-8 flex items-center justify-center text-center">
                     Pregled zahtjeva za povrat novca
+                </Link>
+                <Link to={"/admin/viewReviews"} className="button-style text-3xl w-72 h-20 flex items-center justify-center text-center">
+                    Pregled recenzija sistema
                 </Link>
 
                 <button
