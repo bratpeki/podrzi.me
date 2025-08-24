c45a761c740a106d0e550d38c3842f4e55f52809
bratpeki <pkatic2003@gmail.com>
2025-08-24 18:30:08 +0200

    Dodat DML

---
+++ sql/dml.sql
@@ -0,0 +1,8 @@
+USE pdme;
+
+INSERT INTO `Admin` (`username`, `password`, `owner`)
+VALUES (
+    "admin", -- username
+    "$2a$10$omkBPbTTiU2M3/wr8rAi/utXS9djDy/uOPGGgo6dDeCYALqYU77c.", -- password, Spring-ov default je bcrypt, snaga 10
+    1 -- owner
+);
